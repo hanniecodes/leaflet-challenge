e16a12e9be4c402aa8bcc097d7086bdde38a711f
hanniecodes <hannahgallen@gmail.com>
2022-07-09 10:02:52 -0400

    Legend works! - final

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -66,11 +66,12 @@ function chooseColor(depth) {
     var div = L.DomUtil.create('div', 'legend'),
     depth = [-10, 10, 30, 50, 70, 90];
     div.innerHTML += "<h2 style='text-align: center'>Depth</h2>"
+
   for (var i =0; i < depth.length; i++) {
     div.innerHTML += 
     '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
         depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
-      }
+   }
 
     return div;
   };
@@ -110,6 +111,8 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+
   legend.addTo(myMap);
 
 
